99cb7f580b377eaf79c8625f6547182cf8fd08b4
KarolisBz <G00417529@atu.ie>
2023-03-27 16:19:23 +0100

    Ceaser chiper positive shift fully finished

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 let charToInt = (character) => character.charCodeAt(0);
 let intToChar = (intLetter) => String.fromCharCode(intLetter);
 
+function intIsUppercase(charInt)
+{
+    let upperCase = false;
+
+    // checking if int is inbounds of being a uppercase
+    if(charInt <= 90 && charInt >= 65)
+    {
+        upperCase = true;
+    }
+
+    return upperCase;
+}
+
 // checks if the ascii values is within the bounds of only letters, if it's past the alphabet it'll return to the start of the
 // array with the remaining shifts added onto it.
-function alphabetBounds(charInt)
+function alphabetBounds(charInt, shift)
 {
+    let upperCase = false;
+    
+    // checks if int is uppercase
+    upperCase = intIsUppercase(charInt-shift);
+
+    // if it's uppercase we will change change it to lower case for the conversion (32 is the difference between upper and lowercase on the asci table)
+    if(upperCase)
+    {
+        charInt += 32;
+    }
+
     do
     {
         if(charInt > 122) // if letter int is passed z
@@ -16,6 +40,12 @@ function alphabetBounds(charInt)
             charInt += 26;
         }
     } while((charInt > 122 || charInt < 97) && charInt != 32) // loops as many times as it takes to set letter int into bounds or if it's a space (int 32)
+
+    // we will now return it to uppercase
+    if(upperCase)
+    {
+        charInt -= 32;
+    }
 
     return charInt;
 }
@@ -31,12 +61,11 @@ function caesarChiper(inputText, output)
     for(i = 0; i < inputArray.length; i++)
     {
         let charInt = charToInt(inputArray[i]);
-        console.log(charInt + " letter: " + inputArray[i]);
 
         // adding shift
         charInt += Number(shift);
         // checking bounds
-        charInt = alphabetBounds(charInt);
+        charInt = alphabetBounds(charInt, shift);
         // building the final string to output
         finalOutput += intToChar(charInt);
     }
